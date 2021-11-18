af2d342e885aa20c0a2c37aa706343e0d4e121e5
shishir <ccrsharma7@gmail.com>
2021-11-18 16:56:34 +0100

    config file change

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,8 @@
-// importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js")
-// importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js")
+importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js")
+importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js")
 
-importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
-importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
+// importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
+// importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
 importScripts('app.js');
 
 var firebaseConfig = {
