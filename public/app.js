7838788e182791b1ffe836af8d748a31fa98bec8
shishir <ccrsharma7@gmail.com>
2021-11-18 14:47:48 +0100

    small changes

--- public/app.js
+++ public/app.js
@@ -1,11 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyA9UsApUsATI1c-uzBPpL3vbxbxu4jGJ6s",
     authDomain: "testingnotifications-e13b6.firebaseapp.com",
@@ -15,12 +7,7 @@ const firebaseConfig = {
     appId: "1:769578515843:web:f869a43c9895b6831a4ab6",
     measurementId: "G-JH74H9TSH0"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
+firebase.initializeApp(firebaseConfig);
 
 function subUser() {
     console.log('Hello World!')
