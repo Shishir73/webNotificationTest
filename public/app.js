d6d7120dcf2cd95d4e179ff737eded3b6563555e
shishir <ccrsharma7@gmail.com>
2021-11-18 14:34:16 +0100

    first commit

---
+++ public/app.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9UsApUsATI1c-uzBPpL3vbxbxu4jGJ6s",
+    authDomain: "testingnotifications-e13b6.firebaseapp.com",
+    projectId: "testingnotifications-e13b6",
+    storageBucket: "testingnotifications-e13b6.appspot.com",
+    messagingSenderId: "769578515843",
+    appId: "1:769578515843:web:f869a43c9895b6831a4ab6",
+    measurementId: "G-JH74H9TSH0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+function subUser() {
+    console.log('Hello World!')
+}
