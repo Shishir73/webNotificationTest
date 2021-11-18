750df0b5c74a9ba2f7155fe759e7afc7cb406482
shishir <ccrsharma7@gmail.com>
2021-11-18 16:48:50 +0100

    solving to get the token

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,9 @@
-importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js")
-importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js")
+// importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-app.js")
+// importScripts("https://www.gstatic.com/firebasejs/8.3.0/firebase-messaging.js")
+
+importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
+importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
+importScripts('app.js');
 
 var firebaseConfig = {
     apiKey: "AIzaSyA9UsApUsATI1c-uzBPpL3vbxbxu4jGJ6s",
